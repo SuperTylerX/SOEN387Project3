f43018e7929dedd9c56d81a7d28a51ea8dc8f786
bowen01 <bowen.yang@mail.concordia.ca>
2020-11-06 12:03:43 -0500

    Done search part, add several methods in PostDAO

---
+++ src/main/java/controller/SearchController.java
@@ -0,0 +1,122 @@
+package controller;
+
+import com.google.gson.Gson;
+import dao.PostDAO;
+import model.Post;
+import model.UserManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.Response;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@WebServlet(name = "SearchController")
+public class SearchController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String author = request.getParameter("author");
+        long userID = -1;
+        if (author != null) {
+            userID = UserManager.getInstance().getUserIdByName(author);
+        }
+        String startDate = request.getParameter("startDate");
+        String endDate = request.getParameter("author");
+        String tags = request.getParameter("tags");
+        String[] tagArr = {};
+        if (tags != null) ;
+        {
+            tagArr = tags.split(",");
+        }
+        PostDAO postdao = new PostDAO();
+
+        ArrayList<Post> posts = new ArrayList<>();
+
+        if (userID != -1 && (startDate == null || endDate == null) && tagArr.length == 0) {
+            posts = postdao.readPostsByAutherId(userID);
+            String resultJson = arrToSuccessJson(posts);
+            sendInfo(response, resultJson);
+        } else if (userID == -1 && startDate != null && endDate != null && tagArr.length == 0) {
+            posts = postdao.readPostsByDate(Long.parseLong(startDate), Long.parseLong(endDate));
+            String resultJson = arrToSuccessJson(posts);
+            sendInfo(response, resultJson);
+        } else if (userID == -1 && (startDate == null || endDate == null) && tagArr.length != 0) {
+            for (String s : tagArr) {
+                ArrayList<Post> temp = new ArrayList<>();
+                temp = postdao.readPostsByContent(s);
+                posts.addAll(temp);
+            }
+            String resultJson = arrToSuccessJson(posts);
+            sendInfo(response, resultJson);
+        } else if (userID != -1 && startDate != null && endDate != null && tagArr.length == 0) {
+            posts = postdao.readPostsByAutherIdAndDate(userID, Long.parseLong(startDate), Long.parseLong(endDate));
+            String resultJson = arrToSuccessJson(posts);
+            sendInfo(response, resultJson);
+        } else if (userID == -1 && startDate != null && endDate != null && tagArr.length != 0) {
+            for (String s : tagArr) {
+                ArrayList<Post> temp = new ArrayList<>();
+                temp = postdao.readPostsByContentAndDate(s,Long.parseLong(startDate), Long.parseLong(endDate));
+                posts.addAll(temp);
+            }
+            String resultJson = arrToSuccessJson(posts);
+            sendInfo(response, resultJson);
+        }else if (userID != -1 && (startDate == null || endDate == null) && tagArr.length != 0){
+            for (String s : tagArr) {
+                ArrayList<Post> temp = new ArrayList<>();
+                temp = postdao.readPostsByAutherIdAndTag(userID,s);
+                posts.addAll(temp);
+            }
+            String resultJson = arrToSuccessJson(posts);
+            sendInfo(response, resultJson);
+        }else if(userID != -1 && startDate != null && endDate != null && tagArr.length != 0){
+            for (String s : tagArr) {
+                ArrayList<Post> temp = new ArrayList<>();
+                temp = postdao.readPostsByAll(userID,Long.parseLong(startDate), Long.parseLong(endDate),s);
+                posts.addAll(temp);
+            }
+            String resultJson = arrToSuccessJson(posts);
+            sendInfo(response, resultJson);
+        }else{
+            // no limit return all posts
+//            posts=postdao.readPosts();
+//            String resultJson = arrToSuccessJson(posts);
+//            sendInfo(response, resultJson);
+            //for test only
+            String resultJson=wrongJson();
+            sendInfo(response, resultJson);
+        }
+
+    }
+
+    private String arrToSuccessJson(ArrayList<Post> posts) {
+        HashMap<String, Object> res = new HashMap<>();
+        res.put("status", 200);
+        res.put("data", posts);
+        Gson gson = new Gson();
+        String resultJson = gson.toJson(res);
+        return resultJson;
+    }
+
+    private String wrongJson(){
+        HashMap<String, Integer> res = new HashMap<>();
+        res.put("status", 403);
+        Gson gson = new Gson();
+        String resultJson = gson.toJson(res);
+        return resultJson;
+    }
+
+    private void sendInfo(HttpServletResponse response, String json) throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json);
+    }
+
+
+}
--- src/main/java/dao/PostDAO.java
+++ src/main/java/dao/PostDAO.java
@@ -91,9 +91,7 @@ public class PostDAO {
             e.printStackTrace();
             return false;
         }
-
     }
-
     public boolean checkValidOwner(long userId, int postID) {
         Connection connection = DBConnection.getConnection();
         try {
@@ -181,6 +179,349 @@ public class PostDAO {
 
                 posts.add(p);
 
+            }
+            return posts;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+    public ArrayList<Post> readPostsByAutherId(long post_author_id) {
+        ArrayList<Post> posts = new ArrayList<>();
+        Connection connection = DBConnection.getConnection();
+        try {
+            int postNum = Integer.parseInt(AppConfig.getInstance().POST_NUM);
+            Statement stmt = connection.createStatement();
+            String query = "select * from posts LEFT JOIN attachment on post_attach_id=attach_id where post_author_id=? order by post_modified_date desc limit ?;";
+            PreparedStatement ps = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            ps.setLong(1, post_author_id);
+            ps.setInt(2, postNum);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Post p = new Post();
+                p.setPostID(rs.getInt("post_id"));
+
+                int authorId = rs.getInt("post_author_id");
+                String username = UserManager.getInstance().getUserNameById(authorId);
+                p.setPostAuthorID(authorId);
+                p.setPostAuthorName(username);
+                p.setPostContent(rs.getString("post_content"));
+                p.setPostCreatedDate(rs.getLong("post_created_date"));
+                p.setPostModifiedDate(rs.getLong("post_modified_date"));
+                p.setPostTitle(rs.getString("post_title"));
+
+                if (rs.getInt("attach_id") != 0) {
+                    Attachment att = new Attachment();
+                    att.setAttachID(rs.getInt("attach_id"));
+                    att.setAttachName(rs.getString("attach_name"));
+                    p.setAttachment(att);
+                }
+                posts.add(p);
+            }
+            return posts;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public ArrayList<Post> readPostsByDate(long startDate, long endDate) {
+        ArrayList<Post> posts = new ArrayList<>();
+        Connection connection = DBConnection.getConnection();
+        try {
+            int postNum = Integer.parseInt(AppConfig.getInstance().POST_NUM);
+            Statement stmt = connection.createStatement();
+            String query = "select * from posts LEFT JOIN attachment on post_attach_id=attach_id where post_created_date between ? And ? order by post_modified_date desc limit ?;";
+            PreparedStatement ps = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            ps.setLong(1, startDate);
+            ps.setLong(2, endDate);
+            ps.setInt(3, postNum);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Post p = new Post();
+                p.setPostID(rs.getInt("post_id"));
+
+                int authorId = rs.getInt("post_author_id");
+                String username = UserManager.getInstance().getUserNameById(authorId);
+                p.setPostAuthorID(authorId);
+                p.setPostAuthorName(username);
+                p.setPostContent(rs.getString("post_content"));
+                p.setPostCreatedDate(rs.getLong("post_created_date"));
+                p.setPostModifiedDate(rs.getLong("post_modified_date"));
+                p.setPostTitle(rs.getString("post_title"));
+
+                if (rs.getInt("attach_id") != 0) {
+                    Attachment att = new Attachment();
+                    att.setAttachID(rs.getInt("attach_id"));
+                    att.setAttachName(rs.getString("attach_name"));
+                    p.setAttachment(att);
+                }
+                posts.add(p);
+            }
+            return posts;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public ArrayList<Post> readPostsByContent(String tag) {
+        ArrayList<Post> posts = new ArrayList<>();
+        Connection connection = DBConnection.getConnection();
+        int postNum = Integer.parseInt(AppConfig.getInstance().POST_NUM);
+        try {
+
+            Statement stmt = connection.createStatement();
+            String query = "select * from posts LEFT JOIN attachment on post_attach_id=attach_id where post_content LIKE '%'?'%' or post_title LIKE '%'?'%' order by post_modified_date desc limit ?;";
+            PreparedStatement ps = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, tag);
+            ps.setString(2, tag);
+            ps.setInt(3, postNum);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Post p = new Post();
+                p.setPostID(rs.getInt("post_id"));
+
+                int authorId = rs.getInt("post_author_id");
+                String username = UserManager.getInstance().getUserNameById(authorId);
+                p.setPostAuthorID(authorId);
+                p.setPostAuthorName(username);
+                p.setPostContent(rs.getString("post_content"));
+                p.setPostCreatedDate(rs.getLong("post_created_date"));
+                p.setPostModifiedDate(rs.getLong("post_modified_date"));
+                p.setPostTitle(rs.getString("post_title"));
+
+                if (rs.getInt("attach_id") != 0) {
+                    Attachment att = new Attachment();
+                    att.setAttachID(rs.getInt("attach_id"));
+                    att.setAttachName(rs.getString("attach_name"));
+                    p.setAttachment(att);
+                }
+                posts.add(p);
+            }
+            return posts;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public ArrayList<Post> readPostsByAutherIdAndDate(long post_author_id,long startDate, long endDate) {
+        ArrayList<Post> posts = new ArrayList<>();
+        Connection connection = DBConnection.getConnection();
+        try {
+            int postNum = Integer.parseInt(AppConfig.getInstance().POST_NUM);
+            Statement stmt = connection.createStatement();
+            String query = "select * from posts LEFT JOIN attachment on post_attach_id=attach_id where post_author_id=? and post_created_date between ? And ? order by post_modified_date desc limit ?;";
+            PreparedStatement ps = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            ps.setLong(1, post_author_id);
+            ps.setLong(2, startDate);
+            ps.setLong(3, endDate);
+            ps.setInt(4, postNum);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Post p = new Post();
+                p.setPostID(rs.getInt("post_id"));
+
+                int authorId = rs.getInt("post_author_id");
+                String username = UserManager.getInstance().getUserNameById(authorId);
+                p.setPostAuthorID(authorId);
+                p.setPostAuthorName(username);
+                p.setPostContent(rs.getString("post_content"));
+                p.setPostCreatedDate(rs.getLong("post_created_date"));
+                p.setPostModifiedDate(rs.getLong("post_modified_date"));
+                p.setPostTitle(rs.getString("post_title"));
+
+                if (rs.getInt("attach_id") != 0) {
+                    Attachment att = new Attachment();
+                    att.setAttachID(rs.getInt("attach_id"));
+                    att.setAttachName(rs.getString("attach_name"));
+                    p.setAttachment(att);
+                }
+                posts.add(p);
+            }
+            return posts;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public ArrayList<Post> readPostsByContentAndDate(String tag,long startDate, long endDate) {
+        ArrayList<Post> posts = new ArrayList<>();
+        Connection connection = DBConnection.getConnection();
+        int postNum = Integer.parseInt(AppConfig.getInstance().POST_NUM);
+        try {
+
+            Statement stmt = connection.createStatement();
+            String query = "select * from posts LEFT JOIN attachment on post_attach_id=attach_id where (post_content LIKE '%'?'%' or post_title LIKE '%'?'%' and post_created_date between ? And ?) order by post_modified_date desc limit ?;";
+            PreparedStatement ps = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, tag);
+            ps.setString(2, tag);
+            ps.setLong(3, startDate);
+            ps.setLong(4, endDate);
+            ps.setInt(5, postNum);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Post p = new Post();
+                p.setPostID(rs.getInt("post_id"));
+
+                int authorId = rs.getInt("post_author_id");
+                String username = UserManager.getInstance().getUserNameById(authorId);
+                p.setPostAuthorID(authorId);
+                p.setPostAuthorName(username);
+                p.setPostContent(rs.getString("post_content"));
+                p.setPostCreatedDate(rs.getLong("post_created_date"));
+                p.setPostModifiedDate(rs.getLong("post_modified_date"));
+                p.setPostTitle(rs.getString("post_title"));
+
+                if (rs.getInt("attach_id") != 0) {
+                    Attachment att = new Attachment();
+                    att.setAttachID(rs.getInt("attach_id"));
+                    att.setAttachName(rs.getString("attach_name"));
+                    p.setAttachment(att);
+                }
+                posts.add(p);
+            }
+            return posts;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public ArrayList<Post> readPostsByAutherIdAndTag(long post_author_id,String tag) {
+        ArrayList<Post> posts = new ArrayList<>();
+        Connection connection = DBConnection.getConnection();
+        try {
+            int postNum = Integer.parseInt(AppConfig.getInstance().POST_NUM);
+            Statement stmt = connection.createStatement();
+            String query = "select * from posts LEFT JOIN attachment on post_attach_id=attach_id where (post_author_id=? and post_content LIKE '%'?'%' or post_title LIKE '%'?'%') order by post_modified_date desc limit ?;";
+            PreparedStatement ps = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            ps.setLong(1, post_author_id);
+            ps.setString(2, tag);
+            ps.setString(3, tag);
+            ps.setInt(4, postNum);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Post p = new Post();
+                p.setPostID(rs.getInt("post_id"));
+
+                int authorId = rs.getInt("post_author_id");
+                String username = UserManager.getInstance().getUserNameById(authorId);
+                p.setPostAuthorID(authorId);
+                p.setPostAuthorName(username);
+                p.setPostContent(rs.getString("post_content"));
+                p.setPostCreatedDate(rs.getLong("post_created_date"));
+                p.setPostModifiedDate(rs.getLong("post_modified_date"));
+                p.setPostTitle(rs.getString("post_title"));
+
+                if (rs.getInt("attach_id") != 0) {
+                    Attachment att = new Attachment();
+                    att.setAttachID(rs.getInt("attach_id"));
+                    att.setAttachName(rs.getString("attach_name"));
+                    p.setAttachment(att);
+                }
+                posts.add(p);
+            }
+            return posts;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public ArrayList<Post> readPostsByAll(long post_author_id,long startDate, long endDate,String tag) {
+        ArrayList<Post> posts = new ArrayList<>();
+        Connection connection = DBConnection.getConnection();
+        try {
+            int postNum = Integer.parseInt(AppConfig.getInstance().POST_NUM);
+            Statement stmt = connection.createStatement();
+            String query = "select * from posts LEFT JOIN attachment on post_attach_id=attach_id where (post_author_id=? and (post_content LIKE '%'?'%' or post_title LIKE '%'?'%') and post_created_date between ? And ?) order by post_modified_date desc limit ?;";
+            PreparedStatement ps = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            ps.setLong(1, post_author_id);
+            ps.setString(2, tag);
+            ps.setString(3, tag);
+            ps.setLong(4, startDate);
+            ps.setLong(5, endDate);
+            ps.setInt(6, postNum);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Post p = new Post();
+                p.setPostID(rs.getInt("post_id"));
+
+                int authorId = rs.getInt("post_author_id");
+                String username = UserManager.getInstance().getUserNameById(authorId);
+                p.setPostAuthorID(authorId);
+                p.setPostAuthorName(username);
+                p.setPostContent(rs.getString("post_content"));
+                p.setPostCreatedDate(rs.getLong("post_created_date"));
+                p.setPostModifiedDate(rs.getLong("post_modified_date"));
+                p.setPostTitle(rs.getString("post_title"));
+
+                if (rs.getInt("attach_id") != 0) {
+                    Attachment att = new Attachment();
+                    att.setAttachID(rs.getInt("attach_id"));
+                    att.setAttachName(rs.getString("attach_name"));
+                    p.setAttachment(att);
+                }
+                posts.add(p);
             }
             return posts;
         } catch (SQLException e) {
