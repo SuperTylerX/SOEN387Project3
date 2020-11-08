350014b26641c41eb748d2b58ed5aa17e1b5d6a0
SiaXD <sia1228lee@gmail.com>
2020-11-08 10:59:45 -0500

    Revert "create attachment attempt"
    This reverts commit dc125d4de2664831c976b9933c39210991a0cc6a.

--- src/main/java/controller/AttachController.java
+++ src/main/java/controller/AttachController.java
@@ -1,13 +1,10 @@
 package controller;
 
-import dao.AttachmentDAO;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
 import java.io.IOException;
 
 @WebServlet(name = "AttachController")
@@ -20,13 +17,6 @@ public class AttachController extends HttpServlet {
             return;
         }
         long userId = (long) request.getSession().getAttribute("userId");
-
-        File file=(File)request.getSession().getAttribute("file");
-
-        AttachmentDAO attachmentDAO=new AttachmentDAO();
-        //Haven't figure out how to get name,
-        attachmentDAO.createAttachment("name",file);
-
 
 
     }
