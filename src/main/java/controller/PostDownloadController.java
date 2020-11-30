a6e1e2ca011869a69d1e04c31fb2c1aa61aa3e87
bowen01 <bowen.yang@mail.concordia.ca>
2020-11-30 11:22:54 -0500

    down xml part

--- src/main/java/controller/PostDownloadController.java
+++ src/main/java/controller/PostDownloadController.java
@@ -4,6 +4,7 @@ import dao.PostDAO;
 import model.Post;
 import model.User;
 import model.UserManager;
+import utils.XMLTransformer;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,11 +12,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "postDownloadController")
 public class PostDownloadController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request,response);
+        doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -28,13 +30,18 @@ public class PostDownloadController extends HttpServlet {
         int postId = Integer.parseInt(request.getParameter("postId"));
 
         //search post
-        PostDAO postdao=new PostDAO();
-        Post post=postdao.readPostsByPostId(postId);
-        long groupId=post.getPostGroupID();
+        PostDAO postdao = new PostDAO();
+        Post post = postdao.readPostsByPostId(postId);
+        long groupId = post.getPostGroupID();
         try {
-            if (UserManager.getInstance().checkGroupValidity(userId,groupId)){
-                //todo: turn the post to xml
-            }else{
+            if (UserManager.getInstance().checkGroupValidity(userId, groupId)) {
+                XMLTransformer transformer = XMLTransformer.getInstance();
+                String str = transformer.toXMLString(post);
+                response.setContentType("application/xml");
+                response.setHeader("Content-Disposition", "attachment;filename=" + post.getPostTitle() + ".xml");
+                PrintWriter out = response.getWriter();
+                out.println(str);
+            } else {
                 response.sendError(HttpServletResponse.SC_BAD_REQUEST);
             }
         } catch (Exception e) {
